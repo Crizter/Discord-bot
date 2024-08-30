cb57690f3f809e02df7900acda2c507740ba61a3
Harsh Sharma <harshsharma90866@gmail.com>
2024-08-30 23:12:56 +0530

    Test casese

--- test.js
+++ test.js
@@ -1,1 +1,17 @@
-import { Client,  } from "discord.js";
+import { REST, Routes,Events, SlashCommandBuilder } from 'discord.js';
+import { clientId, guildId, token } from './config';
+import fs from 'node:fs';
+import path from 'node:path';
+import { Client } from 'discord.js';
+const commands = [] ; 
+
+// New instance of rest module 
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.GuildVoiceStates,
+    ],
+})
+const rest = new REST().setToken(process.env.BOT_TOKEN);
